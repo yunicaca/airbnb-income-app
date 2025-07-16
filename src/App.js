e8145baf0d1928a8490dd01136e8ffd64443054e
Yunjia Zhao <yunica@YunjiadeMBP.lan>
2025-07-16 14:23:05 -0700

    fix: manually skip first row in CSV

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import MonthlyDetailAnalysis from './pages/MonthlyDetailAnalysis';
 import Papa from 'papaparse';
 
